1c2a04dda8ae908227248b0b66ff405911a7e89c
Jeff Nielsen <jefftoddnielsen@gmail.com>
2021-01-11 20:54:40 +1100

    Replaced with address form

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Address</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,50 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    <form method="POST">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>Street number</label>
+                                    <input class="form-control" type="text" name="street_number" />
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <div class="form-group">
+                                    <label>Street name</label>
+                                    <input class="form-control" type="text" name="street_name" />
+                                </div>
+                            </div>
+
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>Suburb</label>
+                                    <input class="form-control" type="text" name="suburb" />
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>State</label>
+                                    <input class="form-control" type="text" name="state" />
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <div class="form-group">
+                                    <label>Postcode</label>
+                                    <input class="form-control" type="text" name="postcode" />
+                                </div>
+                            </div>
+
+                            <div class="col-md-12">
+                                <div class="form-group">
+                                    <label>Country</label>
+                                    <input class="form-control" type="text" name="country" />
+                                </div>
+                            </div>
+                        </div>
+
+                        <button class="btn btn-primary" type="submit">Save address</button>
+                    </form>
                 </div>
             </div>
         </div>
