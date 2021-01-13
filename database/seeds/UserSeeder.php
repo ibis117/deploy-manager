26c8679eba72c9ddc694ce1de3d1b1d2bca29e1d
jeff.nielsen <jeff.n@covergenius.com>
2021-01-13 22:36:52 +1100

    Added user seeder

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Demo',
+            'email' => 'demo@itsjeffro.com',
+            'password' => Hash::make('changeme'),
+        ]);
+    }
+}
